10c634e732855546533e59dd2e0f4e10aa882a93
Olivia <olivia.asai@edu.unifil.br>
2022-11-11 21:47:58 -0300

    Inclusão classe ContaSalario

---
+++ atv9/src/ContaSalario.java
@@ -0,0 +1,12 @@
+public class ContaSalario extends ContaComum{
+
+    protected int NumeroConta;
+
+    public int getNumeroConta() {
+        return NumeroConta;
+    }
+
+    public void setNumeroConta(int numeroConta) {
+        NumeroConta = numeroConta;
+    }
+}
